453c3d39aab153dd467c74fdaa269758c4a92f53
lvyoung <15143933787@163.com>
2024-12-04 20:17:52 +0800

    hello world!

--- src/Master.java
+++ src/Master.java
@@ -2,6 +2,6 @@ import java.sql.SQLOutput;
 
 public class Master {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world!");
     }
 }
